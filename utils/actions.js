1310b0cde05c0af4b9de30231b383c3d9dd7fb85
CicadaMusic <team@cicadamusic.net>
2025-12-17 14:27:59 +0200

    added writePeopleToJson

--- utils/actions.js
+++ utils/actions.js
@@ -1,16 +1,26 @@
 import { BASE_URL } from "./config.js";
+import fs from "fs";
+
+function writePeopleToJson(path, jsonData) {
+  fs.writeFile(path, jsonData, "utf8", (error) => {
+    if (error) {
+      console.error("Error writing file:", error);
+      return;
+    }
+
+    console.log("JSON file created successfully!");
+  });
+}
 
 export async function getPeopleList() {
-  
   const res = await fetch(`${BASE_URL}/people`);
   if (!res.ok) {
     throw new Error(`HTTP error! status: ${res.status}`);
   }
   const people = await res.json();
-  console.log(people)
+  writePeopleToJson("data/people.json", JSON.stringify(people));
+  console.log(people);
 }
-
-
 
 export function getTranscriptions() {}
 export function searchPeopleByName() {}
